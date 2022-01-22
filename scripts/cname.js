ead578563205fedda299d0bfb2a7aa0a92c8b000
Palkesh <palkeshbaraiya816@gmail.com>
2022-01-22 20:08:22 +0530

    add: added new cname

--- scripts/cname.js
+++ scripts/cname.js
@@ -11,7 +11,7 @@ const filePath = './out/CNAME'
  * 
  * node script/cname.js domain1 domain2 domain3
  * i.e.
- * node script/cname.js hashirshoaeb.com www.hashirshoaeb.com
+ * node script/cname.js palkesh-odoo-info.com www.palkesh-odoo-info.com
  */
 console.log("Creating CNAME...")
 fs.writeFile(filePath, content, (err) => {
