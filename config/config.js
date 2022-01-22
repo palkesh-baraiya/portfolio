ea539c22af847214dce961350c814d246f160620
Palkesh <palkeshbaraiya816@gmail.com>
2022-01-22 19:44:37 +0530

    add: first commit

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-	name: "Hashir",
+	name: "Palkesh",
 	links: [
 		{
 			title: "About",
@@ -25,8 +25,8 @@ export const navigation = {
 	],
 }
 export const intro = {
-	title: "Hey, I'm Hashir",
-	description: "A Computer Engineer creating mobile apps and static websites.",
+	title: "Hey, I'm Palkesh",
+	description: "A Software Engineer who create ERP, CRM, Sale, Inventory, Accounting, eCommerce, Point of Sale, HR, Websites, Project Management Business apps with Odoo.",
 	image: profile.src,
 	buttons: [
 		{
@@ -36,7 +36,7 @@ export const intro = {
 		},
 		{
 			title: "Resume",
-			link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+			link: "https://drive.google.com/file/d/12scAXH9OBAHBbIN8HEsQKprFToZdGlWt/view?usp=sharing",
 			isPrimary: false,
 		},
 	],
@@ -45,8 +45,8 @@ export const intro = {
 export const about = {
 	title: "Who I am",
 	description: [
-		"I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-		"The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
+		"I graduated from Gujarat Technological University (GTU) in 2016 with a degree in Information Technology Engineering. I kick started my professional career as a Python developer.",
+		"The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on LinkedIn, helping people who are just getting into the space of programming.",
 		"When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
 	],
 }
@@ -55,13 +55,13 @@ export const work = {
 	title: "What I do",
 	cards: [
 		{
-			title: "Mobile App Development",
-			description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+			title: "ERP Engineer",
+			description: "I create ERP, CRM, Sale, Inventory, Accounting, eCommerce, Point of Sale, HR, Project Management Business apps with Odoo.",
 			icons: null,
 		},
 		{
 			title: "Web Development",
-			description: "I create responsive static websites using Reactjs.",
+			description: "I create responsive and static websites.",
 			icons: null,
 		}
 	],
@@ -70,41 +70,41 @@ export const work = {
 export const projects = {
 	title: "Projects",
 	cards: [
-		{
-			title: "StarBook",
-			description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
-			icons: [
-				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-				},
-				{
-					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/star_book",
-				},
-			]
-		},
-		{
-			title: "QuranTalk",
-			description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
-			icons: [
-				{
-					icon: faAppStore,
-					link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-				},
-				{
-					icon: faGooglePlay,
-					link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-				},
-			]
-		},
+		// {
+		// 	title: "StarBook",
+		// 	description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+		// 	icons: [
+		// 		{
+		// 			icon: faAppStore,
+		// 			link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
+		// 		},
+		// 		{
+		// 			icon: faGithub,
+		// 			link: "https://github.com/hashirshoaeb/star_book",
+		// 		},
+		// 	]
+		// },
+		// {
+		// 	title: "QuranTalk",
+		// 	description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+		// 	icons: [
+		// 		{
+		// 			icon: faAppStore,
+		// 			link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
+		// 		},
+		// 		{
+		// 			icon: faGooglePlay,
+		// 			link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
+		// 		},
+		// 	]
+		// },
 		{
 			title: "Portfolio",
 			description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
 			icons: [
 				{
 					icon: faGithub,
-					link: "https://github.com/hashirshoaeb/portfolio",
+					link: "https://github.com/palkesh-baraiya/portfolio",
 				},
 			]
 		},
@@ -113,11 +113,11 @@ export const projects = {
 
 export const contact = {
 	title: "Get in touch",
-	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+	description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at palkeshbaraiya816@gmail.com.",
 	buttons: [
 		{
 			title: "Email Me",
-			link: "mailto:hashirshoaeb@gmail.com",
+			link: "mailto:palkeshbaraiya816@gmail.com",
 			isPrimary: true,
 		},
 		{
@@ -131,35 +131,39 @@ export const contact = {
 // SEARCH ENGINE 
 export const SEO = {
 	// 50 - 60 char  
-	title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-	description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+	title: "Palkesh Baraiya | Software Engineer | Techno-Functional | Odoo | Python | ERP Engineer",
+	description: "I create Business apps and websites. I graduated from Gujarat Technological University (GTU) in 2016 with a degree in Information Technology Engineering.",
 	image: profile.src,
 }
 
 export const links = {
 	image: profile.src,
-	title: "@hashirshoaeb",
-	description: "Computer Engineer | Flutter | Reactjs Developer",
+	title: "@palkesh-baraiya",
+	description: "Software Engineer | Techno-Functional | Odoo | Python | ERP Engineer",
 	cards: [
 		{
 			title: "My website",
-			link: "https://hashirshoaeb.com/",
+			link: "https://palkesh-baraiya.github.io/home",
 		},
 		{
-			title: "QuranTalk App",
-			link: "https://www.qurantalk.app/",
+			title: "Blog",
+			link: "https://odooask.blogspot.com/",
 		},
 		{
-			title: "StarBook App",
-			link: "https://starbook.dev/",
+			title: "Website",
+			link: "https://odoo11.wordpress.com/about/",
+		},
+		{
+			title: "Stack Overflow – Story",
+			link: "https://stackoverflow.com/story/palkesh_baraiya",
 		},
 		{
 			title: "My GitHub",
-			link: "https://github.com/hashirshoaeb/",
+			link: "https://github.com/palkesh-baraiya/",
 		},
 		{
 			title: "My LinkedIn",
-			link: "https://www.linkedin.com/in/hashirshoaeb/",
+			link: "https://www.linkedin.com/in/palkesh-baraiya/",
 		},
 	]
 }
